e35ce090dcc5596ea24c3a97bcc6d1fd73bfdd8f
jsguerrah <68879854+jsguerrah@users.noreply.github.com>
2020-07-31 01:22:43 -0500

    Update Main.java
    Adaptación del Main.java para las pruebas de la clase Punto.java

--- talleres/taller01/Main.java
+++ talleres/taller01/Main.java
@@ -9,17 +9,38 @@ public class Main {
 
         System.out.println();
 
-        Punto p = new Punto(-1, 1);
-        // 0,0
-        System.out.printf("Punto: {%f, %f}\n", p.x(), p.y());
-        // 1*sqrt(2)
-        System.out.println("Radio Polar: " + p.radioPolar());
-        // -45
-        System.out.println("Angulo Polar: " + Math.toDegrees(p.anguloPolar()));
-        // 2
-        System.out.println("Angulo Polar: " + p.distanciaEuclidiana(new Punto(1, 1)));
+        System.out.println("Ingresa la ruta del fichero: ");
+        Scanner s = new Scanner(System.in);
+        String solicitud = s.nextLine();
+        File archivo = new File(solicitud);
 
-        System.out.println();
+        try{
+            Scanner scan = new Scanner(archivo);
+            while(scan.hasNextLine()){
+                Scanner lineaActual  =  new Scanner(scan.nextLine());
+                String copia = lineaActual.next();
+                if(!Character.isLetter(copia.charAt(0))){
+                    long id = Long.parseLong(copia);
+                    double x = Double.parseDouble(lineaActual.next());
+                    double y = Double.parseDouble(lineaActual.next());
+
+                    Punto p = new Punto(x, y);
+                    // 0,0
+                    System.out.printf("Punto: {%f, %f}\n", p.x(), p.y());
+                    // 1*sqrt(2)
+                    System.out.println("Radio Polar: " + p.radioPolar());
+                    // -45
+                    System.out.println("Angulo Polar: " + Math.toDegrees(p.anguloPolar()));
+                    // 2
+                    System.out.println("Distancia euclidiana: " + p.distanciaEuclidiana(new Punto(5, 5))+"\n");
+                }
+            }
+        }
+        catch(FileNotFoundException error){
+            System.out.println(error.getMessage());
+        }        
+
+        System.out.println("Realizado con éxito :)\n");
 
         Fecha f1 = new Fecha(1, 8, 2017);
         Fecha f2 = new Fecha(2, 5, 2016);
