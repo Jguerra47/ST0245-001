247cbfe87cfc5eeac64ec778fc8bda0f06f5c15a
jsguerrah <68879854+jsguerrah@users.noreply.github.com>
2020-07-31 01:29:04 -0500

    Update Punto.java

--- talleres/taller01/Punto.java
+++ talleres/taller01/Punto.java
@@ -1,19 +1,19 @@
 /**
  * La clase Punto tiene la intención de representar coordenadas en el espacio y calcular su distancia.
- * 
-* @author Mauricio Toro, Andres Paez
- * @version 1
+ *
+ * @author Mauricio Toro, Andres Paez, Jacobo Rave, Sebastián Guerra
+ * @version 1.1
  */
 
 public class Punto {
-
     private double x, y;
 
     /**
      * Se inicializan los atributos de clase
      */
     public Punto(double x, double y) {
-
+        this.x=x;
+        this.y=y;
     }
 
     /**
@@ -22,7 +22,7 @@ public class Punto {
      * @return eje coordenado x
      */
     public double x() {
-
+        return this.x;
     }
 
     /**
@@ -30,39 +30,40 @@ public class Punto {
      *
      * @return eje coordenado y
      */
-    public double y() {
-
+    public double y(){
+        return this.y;
     }
 
     /**
      * Método para obtener el radio polar, en este caso se puede obtener por medio del teorema de pitágoras.
-     * 
+     *
      *
      * @return radio polar
      *
      * @see <a href="http://mathworld.wolfram.com/PolarCoordinates.html"> Ver más <a/>
      */
-    
-    public double radioPolar() {
 
+    public double radioPolar(){
+        double radioPolar = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        return radioPolar;
     }
-
 
     /**
      * Método para obtener el ángulo polar, en este caso se puede obtener por medio de la tangente inversa.
-     * 
+     *
      *
      * @return angulo polar
      *
      * @see <a href="http://mathworld.wolfram.com/PolarCoordinates.html"> Ver más <a/>
      */
     public double anguloPolar() {
-
+        double anguloPolar = Math.atan2(y, x);
+        return anguloPolar;        
     }
 
     /**
      * Método para obtener la distacia euclidiana. La distacia
-     * euclidiana o euclídea es la distancia "ordinaria" (que se mediría con una regla) entre 
+     * euclidiana o euclídea es la distancia "ordinaria" (que se mediría con una regla) entre
      * dos puntos de un espacio euclídeo, la cual se deduce a partir del teorema de Pitágoras.
      * En otras palabras es halla el radio polar a la diferencia entre los dos puntos.
      *
@@ -74,8 +75,9 @@ public class Punto {
      * comparar el Punto desde el cual fue llamado.
      * @see <a href="http://mathworld.wolfram.com/Distance.html"> Ver más <a/>
      */
-  
+
     public double distanciaEuclidiana(Punto otro) {
-        
+        double distanciaEuclidiana = Math.sqrt(Math.pow((otro.x - this.x), 2 ) + Math.pow( (otro.y - this.y), 2 ) );
+        return distanciaEuclidiana;
     }
 }
