a356c18ac282a339f581350879897d25e4c20b96
jacevar <63611163+jacevar@users.noreply.github.com>
2020-08-04 11:03:54 -0500

    Update Linea2D.java

--- talleres/taller01/Linea2D.java
+++ talleres/taller01/Linea2D.java
@@ -1,3 +1,4 @@
+package Taller1;
 public class Linea2D{
 	
     private final Punto p1, p2 ;
@@ -16,24 +17,30 @@ public class Linea2D{
     
 	public void EncontrarPuntos() {
 		
-		double y;
-
-		for(double i = p1.x() ; i<=p2.x() ; i++) {
-			
-			y=(pendiente*(i-p1.x()))+p1.y();
+		double y; 
 		
+		
+		if(Double.isInfinite(this.pendiente)) {
 			
+			for(double i = p1.y() ; i<=p2.y() ; i++) {
+				System.out.println("{"+ (int )p1.x() + ", "+ (int) i +"}");}
+		}
+		
+		else {
+			for(double i = p1.x() ; i<=p2.x() ; i++) {
+			y=(pendiente*(i-p1.x())) + p1.y();
+		    
 			if(y%1==0) {
 				System.out.println("{"+ (int )i + ", "+ (int) y +"}");
-			}
+				}
 			else {
-				
-		     y=0;
-			}
+				y=0;
+		      }
 			
-		}
+		 }
 				
-	}
-	
+	   }
+	 
+    }
 	
 }
