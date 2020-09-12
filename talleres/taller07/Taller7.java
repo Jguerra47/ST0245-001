e7ba1d838a61ba52bd58441403c265a5041556c1
jsguerrah <68879854+jsguerrah@users.noreply.github.com>
2020-09-11 23:59:34 -0500

    Update Taller7.java

--- talleres/taller07/Taller7.java
+++ talleres/taller07/Taller7.java
@@ -9,30 +9,30 @@
 public class Taller7
 {  
     public static void main(String args[]){
-    LinkedListJacSeb a = new LinkedListJacSeb();
+        LinkedListJacSeb a = new LinkedListJacSeb();
   
-    a.insert(3,0);
-    a.insert(8,1);
-    a.insert(10,2);
-    a.insert(1,3);
-    a.insert(5,4);
+        a.insert(3,0);
+        a.insert(8,1);
+        a.insert(10,2); //Dato mayor
+        a.insert(1,3);
+        a.insert(5,4);
     
-    int as = encontrarMayor(a);
-    System.out.println("El mayor es: "+ as) ;
+        int as = encontrarMayor(a);
+        System.out.println("El dato mayor de la lista enlazada es: "+ as) ;
     }
     
     public static int encontrarMayor(LinkedListJacSeb listaEnlazada){
-    Node cabeza = listaEnlazada.first();
-    return size(cabeza,0);
+        Node cabeza = listaEnlazada.first();
+        return mayorAux(cabeza,0);
     }
     
-    public static int size(Node nodoActual, int mayor){
-        if(nodoActual==null){
+    public static int mayorAux(Node nodoActual, int mayor){
+        if(nodoActual == null){
             return mayor;
         }
         else{           
-            mayor=Math.max(nodoActual.data,mayor);
-            return size(nodoActual.next,mayor);
+            mayor = Math.max(nodoActual.data, mayor);
+            return mayorAux(nodoActual.next, mayor);
         }
-        }
+    }
 }
