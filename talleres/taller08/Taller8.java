050fb0f7ab5fde812458b69aab0f9b1f8abc9df3
jacevar <63611163+jacevar@users.noreply.github.com>
2020-09-25 20:39:13 -0500

    Create Taller8.java

---
+++ talleres/taller08/Taller8.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+/
+ * La clase Taller8 contiene los métodos solicitados en el taller.
+ * @author Jacobo Rave y Sebastian Guerra
+ * @version 1
+ */
+class Taller8{
+    /
+     * El metodo notacionPolacaInversa representa la forma en que se evaluan las
+     * operaciones aritmeticas en los procesadores
+     * @param la cadena con notacion polaca inversa
+     * @return el resultado de la operacion
+     /
+    public static int notacionPolacaInversa(String s){
+        String simbolos = "+-/";
+        Stack<Integer> pila = new Stack();
+
+        if (s.length() > 2){
+            for (String subcadena: s.split(" ")){
+                if (simbolos.contains(subcadena)){
+                    if (subcadena.equals("+")){
+                        pila.push(pila.pop() + pila.pop()); 
+                    }
+                    if (subcadena.equals("-")){
+                        pila.push(-1 * pila.pop() + pila.pop()); 
+                    }
+                    if (subcadena.equals("*")){
+                        pila.push(pila.pop() * pila.pop()); 
+                    }
+                    if (subcadena.equals("/")){
+                        int numeroDeArriba = pila.pop();
+                        int numeroDeAbajo =  pila.pop();
+                        pila.push(numeroDeAbajo/numeroDeArriba); 
+                    }
+                }
+                else
+                    pila.push(Integer.parseInt(subcadena));
+            }
+        }
+
+        return pila.pop();
+    }
+}
