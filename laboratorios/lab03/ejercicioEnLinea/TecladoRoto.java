193b68b0bdecab8fa4cd643bd9cbf51c540a6d5a
jsguerrah <68879854+jsguerrah@users.noreply.github.com>
2020-09-27 23:49:29 -0500

    Add files via upload

---
+++ laboratorios/lab03/ejercicioEnLinea/TecladoRoto.java
@@ -0,0 +1,57 @@
+import java.util.LinkedList;
+/**
+ * La clase TecladoRoto resuelve el problema del teclado roto con listas enlazadas * 
+ * @author Jacobo Rave, Sebastian Guerra
+ * @version 1
+ */
+public class TecladoRoto{
+    /**
+     * El metodo mostrarEnPantalla muestra como queda el texto despues de haber sido escrito en un teclado roto.
+     * 
+     * @param palabra secuencia de texto escrito
+     * @return como se ve en pantalla ese texto
+     **/
+    public static String mostrarEnPantalla(String palabra){
+        LinkedList<String> lista = new LinkedList<>();
+        StringBuilder cadena = new StringBuilder("");
+
+        boolean botonInicio = false;
+        for(int i = 0; i < palabra.length(); i++){
+            char caracter = palabra.charAt(i);
+            if(caracter == '[' || caracter == ']'){
+                if(!botonInicio){
+                    lista.addLast(cadena.toString());
+                }
+                else{
+                    lista.addFirst(cadena.toString()); 
+                }      
+
+                if(caracter == '['){
+                    botonInicio = true;
+                }
+                else{
+                    botonInicio = false;
+                }
+
+                cadena = new StringBuilder("");
+            }
+            else{
+                cadena.append(caracter);
+            }
+        }
+
+        if(!botonInicio){
+            lista.addLast(cadena.toString());
+        }
+        else{
+            lista.addFirst(cadena.toString());
+        }
+
+        String enPantalla = "";
+        while(lista.size() > 0){
+            enPantalla = enPantalla + lista.pollFirst();
+        }
+
+        return enPantalla;
+    }
+}
