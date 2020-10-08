448dde12fac37b84c8cdccda5418edd85333b7fe
jsguerrah <68879854+jsguerrah@users.noreply.github.com>
2020-10-08 01:06:34 -0500

    Update Main.java

--- proyecto/codigo/Main.java
+++ proyecto/codigo/Main.java
@@ -1,31 +1,91 @@
-
 import javafx.util.Pair;
 import java.util.ArrayList;
+import java.util.*;
 import java.io.*;
+
 public class Main
 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
         ArrayList<String[]> m = Lector.leerCSV();
         String[][] matriz = new String[m.size()][m.get(0).length];
-        
+
         for (int i = 0; i < matriz.length; i++) {  
             matriz[i] = m.get(i);
         }
-        
-        //Variable 52: Naturaleza_Colegio ("OFICIAL", "NO OFICIAL")
-        //Variable 17: Genero ("M", "F")
-        //Variable 
-        Pair<String[][],String[][]> pareja = dividirUnaMatrizEnDosMatrices(matriz, 52, "OFICIAL");
-        float impureza1 = impurezaGiniDeUnaMatriz(pareja.getKey());
-        float impureza2 = impurezaGiniDeUnaMatriz(pareja.getValue());
-        System.out.println("La impureza de que se cumpla es: " + impureza1);
-        System.out.println("La impureza de que NO se cumpla es: " + impureza2);
+
+        int iteradorColumna = 6;
+        float impurezaPonderada = 0;
+        ArrayList<Object> respuestas = posiblesValores(matriz, iteradorColumna);
+        for(int s = 0; s < matriz.length; s++){
+            if(respuestas.get(s).getClass().getName().equals("java.lang.Float")){
+                float mediaAritmetica = ((Float)respuestas.get(0)).floatValue();
+                System.out.print("Condicion elegida: Promedio--> "+mediaAritmetica);
+                Pair<String[][],String[][]> pareja = dividirUnaMatrizEnDosMatrices(matriz, iteradorColumna, mediaAritmetica);
+                impurezaPonderada = giniPonderado(pareja.getKey(), pareja.getValue(), iteradorColumna);
+                break;
+            }
+            else if(respuestas.get(s).getClass().getName().equals("java.lang.String")){
+                impurezaPonderada = determinarMinimaImpureza(respuestas, matriz, iteradorColumna);
+                break;
+            }
+        }
+        System.out.println("La impureza ponderada es: " + impurezaPonderada);
+    }
+
+    public static float determinarMinimaImpureza(ArrayList<Object> respuestas, String[][] matriz, int iteradorColumna){
+        float minImpureza = 1;
+        for(Object s: respuestas){
+            String condicionEnCadena = (String)s;
+            System.out.println("Condicion elegida: "+condicionEnCadena);
+            Pair<String[][],String[][]> pareja = dividirUnaMatrizEnDosMatrices(matriz, iteradorColumna, condicionEnCadena);
+            minImpureza = (float)Math.min(minImpureza, giniPonderado(pareja.getKey(), pareja.getValue(), iteradorColumna));
+        } 
+        return minImpureza;
+    }
+    public static ArrayList<Object> posiblesValores(String[][] m, int posVariable){
+        ArrayList<Object> respuestasDeVariable =  new ArrayList<>();
+
+        if(!esNumero(m[0][posVariable])){
+            for (String[] fila: m){                
+                String valor = fila[posVariable];
+                if(valor.isEmpty()){
+                    continue;
+                }
+                boolean cond = true;
+                for(Object o: respuestasDeVariable){
+                    if(o.equals(valor)){
+                        cond = false;
+                        break;
+                    }
+                }
+
+                if(cond){
+                    respuestasDeVariable.add(valor);
+                }
+            }
+        }
+
+        else{
+            respuestasDeVariable.add(calcularMediaDeAtributo(m, posVariable));
+        }
+        return respuestasDeVariable;
+    }
+
+    public static float calcularMediaDeAtributo(String[][] m, int posVariable){
+        float sumaDeAtributo = 0;        
+        for (String[] fila: m){
+            if(!fila[posVariable].isEmpty()){
+                sumaDeAtributo += (float)Float.parseFloat(fila[posVariable]);  
+            }
+        }
+        float media = sumaDeAtributo/m.length;
+        return media;
     }
 
     public static Pair<String[][],String[][]> dividirUnaMatrizEnDosMatrices(String[][] m, int posVariable, String valor){
         int varSIEsIgualAlValor = 0;
         int varNOEsIgualAlValor = 0;
-
+        
         for (String[] fila: m){  //for each #1
             if (fila[posVariable].equals(valor)){
                 varSIEsIgualAlValor++;
@@ -36,17 +96,61 @@ public class Main
 
         String[][] matrizVarSIEsIgualAlValor = new String[varSIEsIgualAlValor][m[0].length];
         String[][] matrizVarNOEsIgualAlValor = new String[varNOEsIgualAlValor][m[0].length];
+        System.out.println("Si cumplen: " + matrizVarSIEsIgualAlValor.length);
+        System.out.println("No cumplen: " + matrizVarNOEsIgualAlValor.length);
 
         int fila1 = 0;
         int fila2 = 0;
 
         for(String[] fila: m){ //for each #2
-            if(fila[posVariable].equals(valor)){
-                matrizVarSIEsIgualAlValor[fila1] = fila;                
+            if(fila[posVariable].equals(valor)){                
+                matrizVarSIEsIgualAlValor[fila1] = fila;
                 fila1++;
             }else{
-                matrizVarNOEsIgualAlValor[fila2] = fila;                
-                fila2++;
+                matrizVarNOEsIgualAlValor[fila2] = fila;        
+                fila2++;            
+            }                
+        }
+
+        Pair<String[][],String[][]> parejaMatrices = new Pair(matrizVarSIEsIgualAlValor,matrizVarNOEsIgualAlValor);
+        return parejaMatrices;
+    }
+
+    public static Pair<String[][],String[][]> dividirUnaMatrizEnDosMatrices(String[][] m, int posVariable, float valor) throws Exception{
+        int varSIEsIgualAlValor = 0;
+        int varNOEsIgualAlValor = 0;
+
+        for (String[] fila: m){  //for each #1            
+            float elementoDeLaFila = (float)Float.parseFloat(fila[posVariable]);
+            if (elementoDeLaFila >= valor){
+                varSIEsIgualAlValor++;
+            }else{
+                varNOEsIgualAlValor++;
+            }
+        }
+
+        String[][] matrizVarSIEsIgualAlValor = new String[varSIEsIgualAlValor][m[0].length];
+        String[][] matrizVarNOEsIgualAlValor = new String[varNOEsIgualAlValor][m[0].length];
+        System.out.println("Si cumplen: " + matrizVarSIEsIgualAlValor.length);
+        System.out.println("No cumplen: " + matrizVarNOEsIgualAlValor.length);
+
+        int fila1 = 0;
+        int fila2 = 0;
+
+        for(String[] fila: m){ //for each #2
+            if(esNumero(fila[posVariable])){
+                float elementoDeLaFila = (float)Float.parseFloat(fila[posVariable]);
+                if(elementoDeLaFila >= valor){                
+                    matrizVarSIEsIgualAlValor[fila1] = fila;
+                    fila1++;
+                }else{
+                    matrizVarNOEsIgualAlValor[fila2] = fila;        
+                    fila2++;            
+                }     
+            }
+            else{
+                matrizVarNOEsIgualAlValor[fila2] = fila;        
+                fila2++;     
             }
         }
 
@@ -54,24 +158,49 @@ public class Main
         return parejaMatrices;
     }
 
-    public static float impurezaGiniDeUnaMatriz(String[][] m){
+    public static boolean esNumero(String str) {
+        return str.matches("-?\\d+(\\.\\d+)?");
+    }
+
+    public static float impurezaGiniDeUnaMatriz(String[][] m, int columnaActual){
         int estudiantesWin = 0;
         int estudiantesFail = 0;
-
+        int niIdea = 0;
         for(String[] fila: m){
             if (fila[77].equals("1")){
                 estudiantesWin++;
-            }//fila.length-1
-            else{
+            }
+            else if(!fila[columnaActual].isEmpty()){
                 estudiantesFail++;
             }
+            else
+                niIdea++;
         }
 
-        float proporcionEstudiantesWin = (float)estudiantesWin/m.length;        
-        float proporcionEstudiantesFail = (float)estudiantesFail/m.length;
+        System.out.println("Estos tuvieron exito de la prueba: " + estudiantesWin+" y estos no tuvieron exito de la prueba: " + estudiantesFail);
+        
+        
+        float proporcionEstudiantesWin = (float)estudiantesWin/(estudiantesWin + estudiantesFail);      
+        float proporcionEstudiantesFail = (float)estudiantesFail/(estudiantesWin + estudiantesFail);
 
         float impurezaGini= 1-((float)Math.pow(proporcionEstudiantesWin, 2)+(float)Math.pow(proporcionEstudiantesFail, 2));
-        return impurezaGini;        
+        return impurezaGini;
     }    
+
+    public static float giniPonderado(String[][] matrizIzquierda, String[][] matrizDerecha, int columnaActual){
+        float elGiniDeLaIzquierda = impurezaGiniDeUnaMatriz(matrizIzquierda, columnaActual);
+        float elGiniDeLaDerecha = impurezaGiniDeUnaMatriz(matrizDerecha, columnaActual);
+        float tamañoIzquierda = matrizIzquierda.length;
+        float tamañoDerecha = 0;
+        for(String[] fila: matrizDerecha){
+            if(!fila[columnaActual].isEmpty()){
+                tamañoDerecha++;
+            }
+        }
+        //Ip = (ni*Ii + nd*Id) / (ni + nd)
+        float giniPonderado = ((tamañoIzquierda * elGiniDeLaIzquierda) + (tamañoDerecha * elGiniDeLaDerecha)) /
+            (tamañoIzquierda + tamañoDerecha);
+        return giniPonderado;
+    }
 
 }
