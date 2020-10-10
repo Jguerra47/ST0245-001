1c4e847cf60525cefb9940239a15196230309aba
jacevar <63611163+jacevar@users.noreply.github.com>
2020-10-09 23:49:17 -0500

    Update Taller9.java

--- talleres/taller09/Taller9.java
+++ talleres/taller09/Taller9.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.Set;
+import java.util.Collection;
 /**
  * Dar solucion taller numero 9
  * Se recomienda hacer una clase o metodo main que instancie esta clase de manera que sirva como test
@@ -11,19 +13,22 @@ import java.util.HashMap;
 public class Taller9{ 
 
     public static void main(String args[]){
-      HashMap<String,String> empresas = new HashMap();
-      agregar(empresas,"Google", "Estados Unidos");
-      agregar(empresas,"La locura", "Colombia");
-      agregar(empresas,"Nokia", "Finlandia");
-      agregar(empresas,"Sony", "Japon");
-      System.out.println(buscar(empresas, "Sony"));//return true
-      System.out.println(buscar(empresas, "Nokia"));
-      System.out.println (buscar(empresas, "Yahoo"));
-      System.out.println(buscar(empresas, "Tesla"));
-      System.out.println(contieneValue(empresas, "Colombia"));
-      System.out.println(contieneValue(empresas, "Indonesia"));
+        /*HashMap<String,String> empresas = new HashMap();
+        agregar(empresas,"Google", "Estados Unidos");
+        agregar(empresas,"La locura", "Colombia");
+        agregar(empresas,"Nokia", "Finlandia");
+        agregar(empresas,"Sony", "Japon");
+        System.out.println(buscar(empresas, "Sony"));//return true
+        System.out.println(buscar(empresas, "Nokia"));
+        System.out.println (buscar(empresas, "Yahoo"));
+        System.out.println(buscar(empresas, "Tesla"));
+        System.out.println(contieneValue(empresas, "Colombia"));
+        System.out.println(contieneValue(empresas, "Indonesia"));*/
+        //-----------------------------------------------------
+        Pedrito3();
+        Pedrito4();
     }
-    
+
     /** 
      * pedrito 2
      * @param empresas es el hashmap ya creado donde se guardaran los valores 
@@ -57,4 +62,53 @@ public class Taller9{
     public static boolean contieneValue(HashMap empresas,String value){
         return empresas.containsValue(value);
     } 
+
+    public static void Pedrito3(){
+        HashMap<String,String> empresasPedro = new HashMap();
+        agregar(empresasPedro,"Google", "Estados Unidos");
+        if(buscar(empresasPedro, "Google")){
+            System.out.println(empresasPedro.get("Google"));}
+        if(buscar(empresasPedro, "Motorola")){
+            System.out.println(empresasPedro.get("Motorola"));}
+    }
+
+    public static void Pedrito4(){
+        HashMap<String,String> empresasPedro = new HashMap();
+        agregar(empresasPedro,"Google", "Estados Unidos");
+        agregar(empresasPedro,"La locura", "Colombia");
+        agregar(empresasPedro,"Nokia", "Finlandia");
+        agregar(empresasPedro,"Sony", "Japon");
+
+        Set<String> claves = empresasPedro.keySet();
+
+        Collection<String> paises = empresasPedro.values();
+
+        if(paises.contains("Estados Unidos")){
+
+            for(String clave : claves){
+                String paisActual = empresasPedro.get(clave);
+                if(paisActual.equals("Estados Unidos")){
+                    System.out.println(clave);
+
+                }
+            }
+
+        }
+        else{
+            System.out.println("El pais no esta en el hashmap");
+        }
+        if(paises.contains("India")){
+
+            for(String clave : claves){
+                String paisActual = empresasPedro.get(clave);
+                if(paisActual.equals("India")){
+                    System.out.println(clave);
+
+                }
+            }
+        }
+        else{
+            System.out.println("El pais no esta en el hashmap");
+        }
+    }
 }
