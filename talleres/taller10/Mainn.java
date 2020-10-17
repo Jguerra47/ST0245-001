b827a105a19c05baef0b9f47afb10ecdb3a8ccf2
jacevar <63611163+jacevar@users.noreply.github.com>
2020-10-16 23:50:02 -0500

    Create Mainn.java

---
+++ talleres/taller10/Mainn.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class mainn here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Mainn
+{
+   public static void main(String args[]){
+    BinaryTree holaa = new BinaryTree();
+    
+    System.out.print("digraph G {");
+    holaa.insertar(new Persona(202720579,"juan"));
+    holaa.insertar(new Persona(301851212,"Jacobo"));
+    holaa.insertar(new Persona(301851227,"Daniel"));
+    holaa.insertar(new Persona(202720590,"Dani"));
+    holaa.insertar(new Persona(202401619,"Sant"));
+    holaa.insertar(new Persona(301851267,"son"));
+    holaa.insertar(new Persona(103305023,"Dan"));
+    holaa.insertar(new Persona(202520799,"San"));
+    holaa.recursivePrint();
+    System.out.print("}");
+    
+   
+    
+
+    }
+}
