0f3e8db0c796cad396d0b9914aa76c646a3ba4f3
jsguerrah <68879854+jsguerrah@users.noreply.github.com>
2020-10-31 04:15:14 -0500

    Create Node.java

---
+++ laboratorios/lab04/ejercicioEnLinea/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    public Node left;
+    public Node right;
+    public int num;
+    
+    public Node(int num){        
+        this.left = null;
+        this.right = null;
+        this.num = num;
+    }
+}
